ffbb4692a46661bef80adf91cce83ac498c5e9c1
Gregory Balay <42926397+shaolynne@users.noreply.github.com>
2021-11-05 15:10:44 +0100

    Add files via upload

---
+++ test-javascript/Balay_Gregory_test_javascript_algo_ex2.js
@@ -0,0 +1,15 @@
+var A =[1,2,3,4,5,6,7,8,9];
+var result ="";
+var el =1;
+function f21(A){
+    for(var i=0; i<A.length; i++) {
+        if(el === array[i]) {
+            el++;
+            result ='true';
+        }
+        else{
+            result='false';
+        }
+    }
+    return result;
+}
